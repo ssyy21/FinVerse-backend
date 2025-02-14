f2fbeb41327a61fc1f0fa2fdcd190527dff2eb9f
Sneha Yadav <143681944+ssyy21@users.noreply.github.com>
2025-02-14 15:00:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ app.get("/", (req, res) => {
 
 // Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
     // Remove deprecated options
   })
   .then(() => console.log("MongoDB connected successfully"))
